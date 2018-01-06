c0a1205cd018a64fbb68d53087c6bc129c53c7f3
CPPBETTERC\yuren <highlight011385@gmail.com>
2018-01-07 04:03:49 +0800

    # redo 20180107

--- src/Class/Course.java
+++ src/Class/Course.java
@@ -82,6 +82,47 @@ public class Course {
 		this.showFormat.showGrades(this.numOfTest);
 	}
 	
+	public void getSingleGrade(Integer userId, Integer courseName) {
+		ArrayList<String> id = new ArrayList<String>();
+		Map<String, String>  tmpScore = new HashMap<String, String>();
+		
+
+//		FileReader fr;
+//		int count = 0;
+//		try {
+//			fr = new FileReader("src/" + courseName.toString() + ".txt");
+//			BufferedReader br = new BufferedReader(fr);
+//			try {
+//				while(br.ready()) {
+//					if(count != 0) {
+//						String Line = br.readLine();
+//						String[] split = Line.split(";");
+//						id.add(split[0]);
+//						studentMidScore.put(split[0], split[1]);
+//						studentFinScore.put(split[0], split[2]);
+//					}
+//					count++;
+//				}
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//		for(int i = 1; i < id.size(); i++) {
+//			if ( userId == Integer.parseInt(id.get(i))){
+//				System.out.println("Searching successfully...");
+//				System.out.println("[UserId]\t\t[Mid]\t\t[Fin]");
+//				System.out.println("--------+--------------");
+//				System.out.print(userId + "\t|\t" 
+//				+ studentMidScore.get(id.get(i)) 
+//				+"\t|\t"
+//				+ studentFinScore.get(id.get(i)));
+//				break;
+//			}
+//		}
+	}
+	
 	@Override
 	public String toString() {
 		return "Class: " + String.valueOf(this.number) + " " + this.name + "\nTeacher: " + Main.getUserName(this.teacher);
